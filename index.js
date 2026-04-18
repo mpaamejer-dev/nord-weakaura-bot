0888887ddeca47c4903572e6446f4e7aced68671
mpaamejer-dev <mpaamejer@gmail.com>
2026-04-18 18:06:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
       }
 
       await panelChannel.send({
-        content: '## Klik på knapperne herunder for at downloade Raid Pack WAs og få vist WA-filerne i .txt-format.',
+        content: '## Guide til Raid Packs\n\nFølg guiden herunder for at importere Fojji\'s Raid Packs.\n\n1. Importér [Fojji\'s Raid Pack Classic](https://wago.io/FojjiRaidAnchors-Classic).\n2. Download den nyeste version af [FojjiCore](https://www.curseforge.com/wow/addons/fojjicore).\n3. Tryk på en af knapperne herunder for at få vist WeakAura-koden.\n4. Hent hele filen via "•••", og vælg derefter "Download".\n5. Åbn filen i fx Notesblok, og kopiér indholdet ind i WeakAuras som import.\n6. Det kan tage et øjeblik, og spillet kan fryse. Det er normalt. Vent til importen er færdig.\n7. Reload spillet med `/reload`.\n8. Færdig.',
         components: buildPanelRows(),
       });
 
